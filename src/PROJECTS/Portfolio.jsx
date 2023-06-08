c214dfa950d2c4521b1ebdc78a8c4c57a5de2a90
kiran5112 <kbijarane@gmail.com>
2023-06-08 09:41:04 +0530

    firstcommit

--- src/PROJECTS/Portfolio.jsx
+++ src/PROJECTS/Portfolio.jsx
@@ -1,55 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './style.css'
+import style from './user.module.css';
+
+
+
 
 function Portfolio() {
     return (
-        <div>
+        <div className={style.port}>
             <div>
-                <ul className="main">
+                <ul className={style.main}>
                     <li> <a href="coder image.webp">🧑‍⚖️</a></li>
-                    <li> <a href="#"> HOME</a></li>
-                    <li> <a href="kiran Biarane - Shortcut.lnk"> HIRE ME</a></li>
-                    <li> <a href="coder image.webp">ABAUT</a></li>
-                    <li> <a href="study meterial.txt"> PORTFOLIO</a></li>
+                    <li> <a href="https://kiran5112.github.io/Chocolate-clone/">FRIST-PROJECT</a></li>
+                    <li> <a href="https://www.linkedin.com/in/kiran-randhawa/"> HIRE ME</a></li>
+                    <li> <a href="https://www.linkedin.com/in/kiran-randhawa/">ABAUT</a></li>
+                    <li> <a href="#"> PORTFOLIO</a></li>
                     <li> <a href="#"> SERVICES</a></li>
 
                 </ul>
-                <div className="font">
+                <div className={style.font}>
                     <h2>My Name Is</h2>
                     <h1>Kiran Randhawa</h1>
                     <h2>And I am a <span className="span">React Developer💻</span>
                     </h2>
-                    <button onclick="alert('fetching the data')">Download Cv</button>
+                    <button> <a href="https://www.linkedin.com/in/kiran-randhawa/">DOWNLOAD CV</a></button>
                 </div>
 
 
             </div>
-            <div className="imgg">
-                <div className="box">
+            <div className={style.imgg}>
+                <div className={style.box}>
                     <img src="kirannn.jpeg" />
                 </div>
-
-
             </div>
-            <h2>My projects</h2>
-            <div>        <ul className="main">
-                <li> <Link to="/Movie">MOVIE-TICKET</Link></li>
-                <li> <Link to="/Web">SHOPPING-APP</Link></li>
-                <li> <Link to="/Weather"> WEATHER-APP</Link></li>
-                <li> <Link to="/Tipcl">CALCULATOR</Link></li>
-                <li> <Link to="/Portfolio"> PORTFOLIO</Link></li>
-                <li> <Link to="/Clock"> MINI-PROJECTS</Link></li>
+            <hr />
+            <h1 className={style.text}>My projects</h1>
+            <hr />
+            <div>
 
-            </ul></div>
-            <h2>More information</h2>
-            <ul className="list">
+                <ul className={style.main}>
+                    <li> <Link to="/Movie">MOVIE-TICKET</Link></li>
+                    <li> <Link to="/Web">SHOPPING-APP</Link></li>
+                    <li> <Link to="/Weather"> WEATHER-APP</Link></li>
+                    <li> <Link to="/Clock">CALCULATOR</Link></li>
+                    <li> <Link to="/Portfolio"> PORTFOLIO</Link></li>
+                    <li> <Link to="/Tipcl"> MINI-PROJECTS</Link></li>
+
+                </ul></div>
+            <h2>More Information</h2>
+            <ul className={style.list}>
                 <li>Skills: HTML, CSS, JAVASCRIPT, REACT, REDUX, BOOTSTRAP. </li>
                 <li>Projects: Shopping app, Weather app, Website Deign, Toodo list app, Tip calculator, My portfolio, Watch, Calculator, Music app etc, </li>
                 <li>EMAIl: kbijarane@gmail.com.</li>
                 <li>College: MJ college Jalgoan. </li>
                 <li>Address: At Malegoan In Maharastra. </li>
             </ul>
+
+
         </div>
     )
 }
